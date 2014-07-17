6865ed545b52df1759999af21d8722efaa47c6cc
Ben Campbell <ben@scumways.com>
2014-07-17 17:12:28 +1200

    add tool to show parsed query

---
+++ cmd/qdump/main.go
@@ -0,0 +1,24 @@
+package main
+
+// dump out parsed tree of query passed in on commandline
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"semprini/crummy/query"
+)
+
+func main() {
+	flag.Parse()
+
+	qs := flag.Arg(0)
+	fmt.Println(qs)
+
+	q, err := query.Parse(qs, "default")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(query.DumpTree(q, 0))
+}
